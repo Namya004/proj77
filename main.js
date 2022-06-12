97d94bb1cf5c513366b70e9a9ca2453cd21a4819
Namya004 <107343660+Namya004@users.noreply.github.com>
2022-06-12 13:26:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var sen="My first console project";
+console.log(sen);
+
+var first="Ranbir";
+console.log(first);
+
+var second="Kapoor";
+console.log(second);
+
+var full_name = first.concat(second);
+console.log(full_name);
+
+var length_name = full_name.length;
+console.log(length_name);
+
+var add = 15+20;
+console.log(add);
